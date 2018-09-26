63a9a962075f2254bca49517c8436496fe2c6e62
Daviid Valentino Bernth <davi9400@edu.eal.dk>
2018-09-26 09:01:35 +0200

    ændring pc2

--- git1/Program.cs
+++ git1/Program.cs
@@ -11,7 +11,7 @@ namespace git1
         static void Main(string[] args)
         {
             Console.WriteLine("hello git1");
-            Console.WriteLine("ny ændring på pc1");
+            Console.WriteLine("ny ændring på pc2");
             Console.WriteLine("Endnu en ændring på git2");
             Console.ReadLine();
         }
